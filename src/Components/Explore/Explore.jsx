5715a0858df677c3606e405798cd1d8431b6aaaf
prusotamkrydv78 <prusotamkumaryadav78@gmail.com>
2024-08-31 01:00:12 +0545

    Explore Page almost completed

--- src/Components/Explore/Explore.jsx
+++ src/Components/Explore/Explore.jsx
@@ -1,5 +1,10 @@
 import React, { useContext, useEffect } from "react";
+import "./Explore.css";
 import NavToggleContext from "../../Context/NavToggleContext/NavToggleContext";
+
+import { BsFillHeartFill } from "react-icons/bs";
+
+import { FaComment } from "react-icons/fa";
 
 const Explore = () => {
   const { navToggle, setNavToggle } = useContext(NavToggleContext);
@@ -7,10 +12,48 @@ const Explore = () => {
     setNavToggle(false);
   }, []);
   return (
-    <div>
-      <h1>Explore pages</h1>
+    <div className="content px-[15px] py-[25px] h-lvh">
+      <RowData />
+      <RowData />
+      <RowData />
+      <RowData />
+      <RowData />
     </div>
-  )
-}
+  );
+};
 
-export default Explore
+export default Explore;
+const RowData = () => {
+  return (
+    <div>
+      <div>
+        <HoverEffect />
+      </div>
+      <div>
+        <HoverEffect />
+      </div>
+      <div>
+        <HoverEffect />
+      </div>
+      <div>
+        <HoverEffect />
+      </div>
+      <div>
+        <HoverEffect />
+      </div>
+    </div>
+  );
+};
+const HoverEffect = () => {
+  return (
+    <div className="details flex items-center justify-center gap-8">
+      <div className="text-white flex items-center justify-center gap-1">
+        <BsFillHeartFill />
+        154k
+      </div>
+      <div className="text-white flex items-center justify-center gap-1">
+        <FaComment /> 45
+      </div>
+    </div>
+  );
+};
